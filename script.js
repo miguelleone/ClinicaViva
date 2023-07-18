8211a3472f1ba52f325aed08dc9b852e5319452d
miguel leone <mguelleone@gmail.com>
2023-07-18 16:05:18 -0300

    metatags

--- script.js
+++ script.js
@@ -110,5 +110,18 @@ conveniosRight.onmouseover = function () {
 };
 
 
-
+function copy() {
+    // Get the text field
+    var copyText = document.getElementById("email");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
 
